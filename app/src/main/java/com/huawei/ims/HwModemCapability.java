a01cdaaa6c8946b9f0ecdecfaf37a5e482aca36f
Hackintosh 5 <penn.mackintosh@gmail.com>
2019-03-12 10:06:02 +0000

    Add file that forgot to commit

---
+++ app/src/main/java/com/huawei/ims/HwModemCapability.java
@@ -0,0 +1,48 @@
+package com.huawei.ims;
+
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class HwModemCapability {
+
+    private static final String TAG = "HwModemCapability";
+    private static String MODEM_CAP = SystemProperties.get("persist.radio.modem.cap", "");
+
+    public static boolean isCapabilitySupport(int capability) {
+        boolean z = true;
+        int bcdIndex = capability / 4;
+        int bcdOffset = capability % 4;
+        if (capability < 0 || capability >= 360) {
+            return false;
+        }
+        if (TextUtils.isEmpty(MODEM_CAP)) {
+            MODEM_CAP = SystemProperties.get("persist.radio.modem.cap", "");
+        }
+        try {
+            int bcdValue = convertChar2Int(MODEM_CAP.charAt(bcdIndex));
+            if (bcdValue != -1) {
+                if (((1 << (3 - bcdOffset)) & bcdValue) <= 0) {
+                    z = false;
+                }
+                return z;
+            }
+        } catch (IndexOutOfBoundsException ex) {
+            Log.e(TAG, "isCapabilitySupport " + ex);
+        }
+        return false;
+    }
+
+    private static int convertChar2Int(char origChar) {
+        if (origChar >= '0' && origChar <= '9') {
+            return origChar - 48;
+        }
+        if (origChar >= 'a' && origChar <= 'f') {
+            return (origChar - 97) + 10;
+        }
+        if (origChar < 'A' || origChar > 'F') {
+            return -1;
+        }
+        return (origChar - 65) + 10;
+    }
+}
