fbdb7afb0c0c736c3813e62038db6434498f55d3
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-05-09 19:12:32 +0100

    Add Video Constants from Huawei code

---
+++ app/src/main/java/com/huawei/ims/video/VideoConstants.kt
@@ -0,0 +1,38 @@
+/*
+ * This file is part of HwIms
+ * Copyright (C) 2019 Penn Mackintosh
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.huawei.ims.video
+
+object VideoConstants {
+    const val EN_VSC_EVENT_PARAM_READY = 1
+    const val EN_VSC_EVENT_PEER_RESOLUTION_CHANGE = 5
+    const val EN_VSC_EVENT_PLAYER_START = 3
+    const val EN_VSC_EVENT_PLAYER_STOP = 4
+    const val EN_VSC_EVENT_START_READY = 2
+    const val EN_VSC_EVENT_VIDEO_QUALITY = 6
+    const val EN_VSC_VPARA_LOCAL_MIRROR = 8
+    const val EN_VSC_VPARA_LOCAL_ROTATE = 1
+    const val EN_VSC_VPARA_NEGO_FPS = 3
+    const val EN_VSC_VPARA_NEGO_HEIGHT = 4
+    const val EN_VSC_VPARA_NEGO_WIDTH = 5
+    const val EN_VSC_VPARA_PEER_HEIGHT = 6
+    const val EN_VSC_VPARA_PEER_WIDTH = 7
+    const val EN_VSC_VPARA_QUALITY_LEVEL = 9
+    const val EN_VSC_VPARA_REMOTE_ROTATE = 2
+    const val VT_FAILED = -1
+    const val VT_OK = 0
+}
